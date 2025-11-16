7b8d019f84ba96c993c86618897f675f0931d932
saskiaalifah <saskiaalifah2006@gmail.com>
2025-11-16 10:35:18 +0700

    PRAKTIKUM 4

---
+++ Praktikum4/Magazine.java
@@ -0,0 +1,14 @@
+
+public class Magazine extends Book {
+    private String category;
+
+    public Magazine(String title, String author, String category) {
+        super(title, author);
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
+
